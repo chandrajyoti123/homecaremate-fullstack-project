69ee0374f6382ed0ef4afe45fdeb9b74ed107899
samikshashahare <samikshashahare953@gmail.com>
2023-12-03 21:28:31 +0530

    created home page

---
+++ client/src/components/Card/Card.js
@@ -0,0 +1,34 @@
+const whyUsCard =  [
+    {
+        logo:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvJcSAGmy51MpY4Y9DgVT1hP1Y1kw57dWjDcd_lP9ksqsRTahv',
+        name:'Guarantee Service',
+        description: 'We offer guaranteed service. We hire the most experienced and qualified candidates to provide high-quality service.'
+    },
+    {
+        logo:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvJcSAGmy51MpY4Y9DgVT1hP1Y1kw57dWjDcd_lP9ksqsRTahv',
+        name:'Free Replacement',
+        description:'If the maid leaves for any reason within the contract period, we will replace with another maid at no cost.'
+    },
+    {
+        logo:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvJcSAGmy51MpY4Y9DgVT1hP1Y1kw57dWjDcd_lP9ksqsRTahv',
+        name:'Safe and reliable',
+        description:'Your safety is our first priority, so we hire the best people we can trust to serve our customers. We check the backgrounds of candidates and also check their references.'
+    },
+    {
+        logo:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvJcSAGmy51MpY4Y9DgVT1hP1Y1kw57dWjDcd_lP9ksqsRTahv',
+        name:'Multiple Options',
+        description:'Watch as many interviews as you want and choose the profiles that suit you. Communicate your requirements to the candidate before confirmation.'
+    },
+    {
+        logo:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvJcSAGmy51MpY4Y9DgVT1hP1Y1kw57dWjDcd_lP9ksqsRTahv',
+        name:'High Quality Service',
+        description:'We provide the highest quality services by exceeding your expectations. Only qualified maids are registered with us.'
+    },
+    {
+        logo:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvJcSAGmy51MpY4Y9DgVT1hP1Y1kw57dWjDcd_lP9ksqsRTahv',
+        name:'Refund Policy',
+        description:'It is simple: either you get what we promise or we willl pay you back. Call us and check our terms and conditions for our refund policy.'
+    }
+]
+
+export default whyUsCard;
--- client/src/view/Home/Home.js
+++ client/src/view/Home/Home.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react';
+import './Home.css';
+import maid from './../Home/maid.png';
+import {Card} from '../../components/Card/Card.js'
+import {WorksCard} from '../../components/WorksCard/WorkCard.js'
+
 
 export default function () {
+  const [howItWorks, setHowItWorks] = useState(Card)
+  const [whyUs, setWhyUs] = useState(WorksCard)
+
   return (
-    <div>
-      this home page
+    <div className='home-main-div'>
+      <div className='navbar'>
+        <h2>HomePage</h2>
+      </div>
+
+      <div>
+        <img src={maid} className='home-img' />
+        <div>
+          <h1 className='home-desc-1'>How It Works</h1>
+          <div>
+            {
+              Card.map((Card, index) => {
+                const { logo, name, description } = Card;
+                return<div>
+                  <Card key={index} img={logo}  name={name} description={ description} />
+                </div>
+
+              })
+            }
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
